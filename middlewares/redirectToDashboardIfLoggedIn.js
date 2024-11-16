68277e07e707721d67c254a05ed7b6019bf3d104
Saman22 <samanjoodakiofficial@gmail.com>
2024-11-16 22:24:58 +0330

    Fixed redirect to dashboard and authentication routes

--- middlewares/redirectToDashboardIfLoggedIn.js
+++ middlewares/redirectToDashboardIfLoggedIn.js
@@ -1,7 +1,32 @@
-exports.redirectToDashboardIfLoggedIn = async (req, res, next) => {
-    if (!req.session.userId) {
-        return res.redirect('/auth/login');
-    }
+const User = require('../models/User');
 
-    next();
+exports.redirectToDashboardIfLoggedIn = async (req, res, next) => {
+    try {
+        if (req.path === '/auth/login') {
+            return next();
+        }
+
+        const userId = req.session.userId;
+
+        if (!userId) {
+            return res.redirect('/auth/login');
+        }
+
+        const user = await User.findById(userId);
+
+        if (!user) {
+            res.clearCookie('connect.sid');
+            return req.session.destroy((err) => {
+                if (err) {
+                    console.error('Error destroying session:', err);
+                    return next(err);
+                }
+                return res.redirect('/auth/login');
+            });
+        }
+
+        next();
+    } catch (error) {
+        console.error(error.message);
+    }
 };
